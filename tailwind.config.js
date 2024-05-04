7b2c9ef328a5e9d383460d28bc07a8e3083c28c0
khanloo <m.nadirkhanloo1380@gmail.com>
2024-05-04 18:06:43 +0330

    add font vazir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Vazir'],
+      },
+    },
   },
   plugins: [],
 }
