8ca3ba97b6f1062701461caec3ec0638350b1922
khanloo <m.nadirkhanloo1380@gmail.com>
2024-05-05 19:28:28 +0330

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {
       fontFamily: {
-        sans: ['Vazir'],
+        sans: ["Vazir"],
       },
     },
   },
   plugins: [],
-}
+};
